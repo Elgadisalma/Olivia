3e322ea0114c68076893257755b9bd7fd417412a
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-05 22:49:18 +0100

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+export default function Footer()
+{
+    return(
+        <>
+        <div className="footer-Container">
+            <footer class="font-sans relative pt-5 pb-3 md:pb-6 ">
+                <hr class="my-3 md:my-6 border-gray-400" />
+                <div class="flex flex-wrap items-center md:justify-between justify-center">
+                    <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+                        <div class="text-sm text-gray-600 dark:text-gray-400 py-1">
+                            &copy;
+                            <span id="year">2024</span>
+                            <a href="https://github.com/ahampriyanshu" class="hover:underline hover:text-gray-900">Olivia</a>.
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            </div>
+        </>
+    )
+}
