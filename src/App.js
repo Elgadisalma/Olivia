6b9a893c18a208aefed8f17ab9b1280134a70a78
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-05 22:49:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route  } from "react-router-dom";
+import Navbar from './components/navbar'
 import Home from './components/home'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
-
+import Contact from './components/contact'
+import Footer from './components/footer'
 
 
 export default function App() {
+
+  const [pointer,setPointer] = useState(8);
+  
   return (
     <Router>
-
+    <>
+      <Navbar />
       <Switch>
-        <Route path="/">
-          <Home/>
-        </Route>
+        <Route path="/" exact component={Home} />
+        <Route path="/contact" component={Contact} />
       </Switch>
-
-    </Router>
+      <Footer />
+    </>
+  </Router>
   )
 }
 
